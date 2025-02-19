5e20d74ab0e2fa762da7f489909a3db4f3ba7ec7
Bonzagni-Giorgio <78437085+Bonzagni-Giorgio@users.noreply.github.com>
2025-02-19 22:52:53 +0100

    fixed prolog

---
+++ Prolog/huffman-codes.pl
@@ -0,0 +1,13 @@
+
+hucodec_decode(Bits, HuffmanTree, Message)
+
+hucodec_encode(Bits, HuffmanTree, Message)
+
+hucodec_encode_file(Filename, HuffmanTree, Bits)
+
+hucodec_generate_huffman_tree(SymbolsAndWeights, HuffmanTree)
+
+hucodec_generate_symbol_bits_table(HuffmanTree, SymbolBitsTable)
+
+hucodec_print_huffman_tree(HuffmanTree)
+
